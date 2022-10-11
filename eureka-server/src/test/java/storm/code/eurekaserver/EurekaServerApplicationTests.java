361cc5572dbbe743a02271a51c6fb8e905603760
Asep Saputra <v.asep.saputra@adira.co.id>
2022-10-11 16:18:44 +0700

    Init

---
+++ eureka-server/src/test/java/storm/code/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package storm.code.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
